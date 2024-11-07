f318c0eb9cb739a037fc2c355ab91f9bc769b05a
m4yk0n <maykon.rodrigues@sptech.school>
2024-11-07 18:12:32 -0300

    Exercícios feitos e testados

--- src/main/java/school/sptech/Figura.java
+++ src/main/java/school/sptech/Figura.java
@@ -1,4 +1,35 @@
 package school.sptech;
 
-public class Figura {
+public abstract class Figura {
+
+    private String cor;
+    private Integer espessura;
+
+    public Figura() {}
+
+    public abstract Double calcularArea();
+
+    @Override
+    public String toString() {
+        return "Figura{" +
+                "cor= '" + cor + '\'' +
+                ", expessura= " + espessura +
+                '}';
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getEspessura() {
+        return espessura;
+    }
+
+    public void setEspessura(Integer expessura) {
+        this.espessura = expessura;
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,56 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+
+    private List<Figura> figuras = new ArrayList<>();
+
+    public Imagem() {}
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaAreas = 0.0;
+
+        for (Figura figura : figuras) {
+            somaAreas += figura.calcularArea();
+        }
+
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> maioresVinte = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20)
+                maioresVinte.add(figura);
+        }
+
+        return maioresVinte;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura> quadrados = new ArrayList<>();
+
+        for(Figura figura : figuras) {
+            if (figura instanceof Quadrado)
+                quadrados.add(figura);
+        }
+
+        return quadrados;
+    }
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        this.figuras = figuras;
+    }
+
 }
